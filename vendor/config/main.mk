47062f23420ca2d9a5f2985b248d450ed1988f76
phaktor <panxer3@gmail.com>
2021-03-02 09:22:19 -0500

    Update main.mk

--- vendor/config/main.mk
+++ vendor/config/main.mk
@@ -1,16 +1,14 @@
 # do not rename this or change path
 DEVICE_PACKAGE_OVERLAYS += vendor/custom/vendor/overlay/common
 
-# MicroG
-PRODUCT_PACKAGES += \
-    GmsCore \
-    GsfProxy \
-    FakeStore \
-    com.google.android.maps.jar
-
 # Seedvault
 PRODUCT_PACKAGES += \
     Seedvault
+    
+# AuroraStore
+PRODUCT_PACKAGES += \
+    AuroraStore \
+    AuroraServices
 
 # Fix for Google Camera
 PRODUCT_COPY_FILES += \
