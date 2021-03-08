5426d8b524806b5875a9061e9b23817bf7f61e96
phaktor <panxer3@gmail.com>
2021-03-08 00:18:12 -0500

    Update main.mk

--- vendor/config/main.mk
+++ vendor/config/main.mk
@@ -9,6 +9,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     TrichromeChrome \
     TrichromeWebView
+    
+# Active Edge
+PRODUCT_PACKAGES += \
+    ElmyraService
 
 # Seedvault
 PRODUCT_PACKAGES += \
